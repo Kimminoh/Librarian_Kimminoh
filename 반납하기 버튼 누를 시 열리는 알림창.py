8c8c28e2b67fb12a081e43994b71c99fc898ecaf
namgangmin <106224189+namgangmin@users.noreply.github.com>
2022-06-04 15:19:16 +0900

    Update and rename 반납하기 버튼 누를 시 열리는 알림창.py to returnbotton click.py
    반납하기 버튼 누를 시 열리는 알림창 초안입니다

--- 반납하기 버튼 누를 시 열리는 알림창.py
+++
@@ -1,8 +0,0 @@
-from tkinter import*
-from tkinter import messagebox
-
-def returnbook:
-    messagebox.askyesno(" ",'"불멸의 이순신"을 반납하시겠습니까? 반납 예정일: 2022-03-11")
-                        # '불멸의 이순신' 부분을 Book_TITLE, '2022-03-11' 부분을 RENT_RDATE로
-                        # format
-    
